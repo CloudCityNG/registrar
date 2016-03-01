e09a08fef00e081fcc106a31cf88201fcc09fd8d
Jessica Fix and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-01 08:39:38 -0800

    set up initial files for Student class

---
+++ tests/StudentTest.php
@@ -0,0 +1,15 @@
+<?php
+    require_once __DIR__ . '/../src/Student.php';
+
+    class StudentTest extends PHPUnit_Framework_TestCase
+    {
+        function test_methodToTest_inputDescription()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+ ?>
